1e4de97d84c6e843ccaa56a9a31f4f5ae9eac286
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-01-29 14:44:16 +0100

    Add files via upload

---
+++ tirage au sort prenom.py
@@ -0,0 +1,9 @@
+import random
+
+liste_etu=["Alexis","Aurélien","Antoine","Abder","Omid","Alicia","Thomas","Baptiste","Romane","Pauline","Axel","Paul","Sylvain","Guillaume","Yvan","Poupou","Max","Salim","Elhadji","Hervé","Félicien","Thomas"]
+
+indice_alea=random.randrange(1,len(liste_etu))
+
+
+print(liste_etu[indice_alea])
+
